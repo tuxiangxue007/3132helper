0556e70ce1210af9ddaf7b2f186ca700b2cdd8d1
DAA <tuxiangxue@163.com>
2018-08-31 09:17:54 +0800

    Initial Commit

---
+++ 3132helper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3132helper
+//
+//  Created by mac on 2018/8/31.
+//  Copyright © 2018年 DAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3132helper/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  3132helper
+//
+//  Created by mac on 2018/8/31.
+//  Copyright © 2018年 DAA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
